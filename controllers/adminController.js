2f02374b79a5e6ae1b236bdb9df003b9d8a0953c
Kelechi Okoronkwo <kelechi.webdev@gmail.com>
2023-05-12 17:59:03 +0100

    Admin Page 2

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,10 +5,7 @@ exports.createOrder = async (req, res, next) => {
   console.log(req.body);
   try {
     const Order = await Item.create(req.body);
-    res.status(200).json({
-      message: "Message",
-      Order,
-    });
+    res.redirect("/admin/order");
   } catch (err) {
     console.log(err);
   }
@@ -17,10 +14,7 @@ exports.getOrder = async (req, res, next) => {
   console.log(req.body);
   try {
     const Order = await Item.find();
-    res.status(200).json({
-      message: "Message",
-      Order,
-    });
+    res.render("orders", { layout: "./innerLayouts", orders: Order });
   } catch (err) {
     console.log(err);
   }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,10 @@ const express = require("express");
 const Router = express.Router();
 const adminController = require("./../controllers/adminController");
 
+Router.get("/new-order", (req, res) => {
+  res.render("newOrder", { layout: "./innerLayouts" });
+});
+Router.get("/order", adminController.getOrder);
 Router.post("/new-order", adminController.createOrder);
 Router.post("/addhistory", adminController.addHistory);
 Router.get("/orders", adminController.getOrder);
